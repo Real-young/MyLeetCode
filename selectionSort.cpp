8328cc909479c2c4565a53623755f35fddd31b68
Real-young <14049575@qq.com>
2017-09-06 12:28:56 +0800

    选择排序

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -10,8 +10,8 @@ void selectionSort (int arr[], int n) {
         for (int j = i+1; j < n;j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
-                swap(arr[i],arr[minIndex]);
             }
+			swap(arr[i],arr[minIndex]);
         }
     }
 }

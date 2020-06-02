10cc5606f4850963d19a0cc9833d07def7ec842b
booming <yangboming@codemao.cn>
2020-06-02 15:34:38 +0800

    quciksort2 add note

--- quickSort/quickSort2Ways.h
+++ quickSort/quickSort2Ways.h
@@ -12,29 +12,29 @@ using namespace std;
 template <typename  T>
 int __partition2(T arr[], int l, int r) {
 // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
-    swap( arr[l] , arr[rand()%(r-l+1)+l] );
+    swap( arr[l] , arr[rand()%(r-l+1)+l] ); // 随机取一个放在最前面
     T v = arr[l];
 
 
     // arr[l+1...i) <= v; arr(j...r] >= v
-    int i = l + 1, j = r;
+    int i = l + 1, j = r;  // 左边从 l+1 开始。 右边从最右边 r 开始
     while (true) {
         while (i <= r && arr[i] < v)
-            i++;
+            i++;  // 左边的值小。 直接不处理。i++
 
         while (j > l + 1 && arr[j] > v)
-            j--;
+            j--;  // 右边的值大。 直接不处理。j--
 
-        if (i > j)
+        if (i > j) // 越界 break
             break;
 
-        swap(arr[i], arr[j]);
+        swap(arr[i], arr[j]);  // 跳出 while 循环的时候说明 左边的值 比目标值大 右边的值比目标值小 直接 swap 交换 继续循环
         i ++;
         j --;
     }
 
-    swap(arr[i], arr[j]);
-    return j;
+    swap(arr[i], arr[j]); // 循环到最后 最后交换
+    return j;   
 
 }
 
